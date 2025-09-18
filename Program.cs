4adda95a2261465d4a4e6da8b9299e2cf54ed4a4
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:08:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ModificadorRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
